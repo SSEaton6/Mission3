1823464a3be3d239863f22c34a50c154ef868c8f
Sidney Eaton <sideaton@byu.edu>
2023-01-25 14:41:16 -0700

    Fixing and adding comments

--- Mission3/Program.cs
+++ Mission3/Program.cs
@@ -7,9 +7,11 @@ namespace Mission3
         
         static void Main(string[] args)
         {
-            string[] boardArray = {"0","1","2", "3", "4", "5", "6", "7", "8", "9"};
+            //initializes the array to keep track of the board
+            string[] boardArray = {"0","1","2", "3", "4", "5", "6", "7", "8"};
+            //this keeps track of whose turn it is
             int playerNum = 1;
-           
+           //keeps track of the turns. once it hits 9, if there is no winner, it is a tie
             int turn = 0;
             string message = ""; // while winner = 0, the game will continue to play. 1 means there's a winner. -1 is a draw.
 
@@ -18,28 +20,36 @@ namespace Mission3
             Support newSupport = new Support();
             newSupport.PrintBoard(boardArray);
 
+            // do while loop to play the game while there isn't a winner
             do
             {
                 while (playerNum == 1) 
                 {
                     Console.WriteLine("Player 1, Enter a number between 0 and 8");
                     int playerchoice = Convert.ToInt32(Console.ReadLine());
-
-                    if (Convert.ToInt32(boardArray[playerchoice]) == playerchoice)
+                    // if else loop to check if player's choice is open or not
+                    
+                    if (boardArray[playerchoice] == Convert.ToString(playerchoice))
                     {
+                        //player 1 is X
                         boardArray[playerchoice] = "X";
+                        //changes the turn to player 2
                         playerNum = 2;
+                        // increments the turn counter
                         turn++;
+                        // passes the board and checks for a win
                         message = newSupport.DetermineWinner(boardArray, turn);
                     }
                     else
                     {
                         Console.WriteLine("That spot is already taken. Please choose another number");
                     }
-                    Console.Clear();
-                    newSupport.PrintBoard(boardArray);
+                    // clears the console so that it just shows one board at a time
+                    
                 }
-                
+                Console.Clear();
+                newSupport.PrintBoard(boardArray);
+                // checks the message variable passed from support class. If there is a winner, it breaks out of the loop.
                 if (message == "X's are the winner!" || message == "It was a Tie!")
                 {
                     break;
@@ -48,11 +58,16 @@ namespace Mission3
                 {
                     Console.WriteLine("Player 2, Enter a number between 0 and 8");
                     int playerchoice = Convert.ToInt32(Console.ReadLine());
-                    if (Convert.ToInt32(boardArray[playerchoice]) == playerchoice)
+                    if (boardArray[playerchoice] == Convert.ToString(playerchoice))
+                    
                     {
+                        // player 2 is O
                         boardArray[playerchoice] = "O";
+                        // changes the turn back to player 1
                         playerNum = 1;
+                        //increments the turn counter
                         turn++;
+                        // passes the board and checks for a win
                         message = newSupport.DetermineWinner(boardArray, turn);
 
                     }
@@ -60,12 +75,14 @@ namespace Mission3
                     {
                         Console.WriteLine("That spot is already taken. Please choose another number");
                     }
-                    Console.Clear();
-                    newSupport.PrintBoard(boardArray);
+                    
                 }
-                
+                Console.Clear();
+                newSupport.PrintBoard(boardArray);
+
             }
             while (message == "") ;
+            // prints the final board and announces winner
             Console.WriteLine(message);
             newSupport.PrintBoard(boardArray);
         }
